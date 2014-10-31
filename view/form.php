f0c9963f6a1fc2a63cb526c14434c9f152032ecc
Carla kesh <Cocokechichian@gmail.com>
2014-10-31 16:18:39 -0700

    /.

---
+++ view/form.php
@@ -0,0 +1,18 @@
+<h1> Create blog post</h1>
+<!--The form tag is used to create an HTML form for user input.-->
+<form> 
+<!-- this div is for title -->
+<div>
+<!-- The <label> tag defines a label for an <input> element.The <label> element does not render 
+as anything special for the user. However, it provides a usability improvement for mouse users,
+ because if the user clicks on the text within the <label> element, it toggles the control.
+The for attribute of the <label> tag should be equal to the id attribute of the 
+related element to bind them together. --> 
+	<label for="title"> Title : </label>
+</div>
+<!-- this div is for post -->
+<div>
+	
+</div>
+
+</form>
