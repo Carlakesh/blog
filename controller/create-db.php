32c16c23625d98087629704538fa520c50c2c5e7
Carla kesh <Cocokechichian@gmail.com>
2014-10-31 15:53:49 -0700

    ..

--- controller/create-db.php
+++ controller/create-db.php
@@ -22,14 +22,17 @@
 //checking if data base excists or not 
  
 if(!$exists) {
+	//sends comands to data base
 	$query = $connection->query ("CREATE DATABASE $database");
 //checking if query is created successfully . 
 if ($query) {
+	//checking if data base is created
 	echo "<p>successfully created a database: " . $database . "</p>";
 }
 
 }
 //else statement 
+// if database exists
 else{
 	echo "<p>datebase already exists</p>";
 }
@@ -37,23 +40,29 @@ else{
 //creating a table
 //Table functions 
 $query = $connection->query("CREATE TABLE posts ("
+	//null indicates that when ever we create a new blog post the id cant be known and id cant be empty
 	//each blog post needs an id
 	. "id int (11) NOT NULL AUTO_INCREMENT,"
 	// storing the title 
 	. "title varchar (255) NOT NULL,"
 	//blog post texts 
 	. "post text NOT NULL,"
+	//adding primary key
 	. "PRIMARY KEY (id))") ;
+//1 means there are 11 values
    
 
 //checking if statement true or false
 if($query) {
+	//statement is true
 	echo "<p> successfully created table : posts</p>";
 }
 //else statement
 else {
+	//error statement 
 	echo "<p> $connection->error </p>";
 }
 //connection closes . 
  $connection->close();
+ 
  
--- index.php
+++ index.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
 <?php 
 //calls the file that you want 
 //DIR to get out of the folder and to get into another folder and get into the file u want
+	require_once(__DIR__ . "/view/header.php");
+	//accessing the navigation file from the view file
+	require_once(__DIR__ . "/view/navigation.php");
+
+	//accessing the file create-db from the folder controller 
 	require_once(__DIR__ . "/controller/create-db.php");
+	//accessing footer.php
+	require_once(__DIR__ . "/view/footer.php");
 
 ?> 
-</body>
-</html> 
