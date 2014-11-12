73855c2a46e1eaac8f99f9762089068dd38f38f2
Carla kesh <Cocokechichian@gmail.com>
2014-11-12 12:23:47 -0800

    finished video #24 , Quering the database to submit a blog post, when submitting a blog post, it straight goes to your local host successfully

--- controller/create-post.php
+++ controller/create-post.php
@@ -2,15 +2,25 @@
 //calling the right file
 require_once(__DIR__ . "/../model/database.php");
 //connection variable database
-$connection = new mysqli($host , $username, $password, $datebase);
+$connection = new mysqli($host, $username, $password, $database);
 //storing the text into a variable , input is to filter the input making sure no errors with the input.
 // Input post is to send the info to us so we can recieve it.
 //Sanatize string is to make sure no errors are going on
 // the code is to receive the info 
 	$title = filter_input(INPUT_POST, "title" , FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST , "post" , FILTER_SANITIZE_STRING);
-//echo to see if the steps above are working correctly .
-echo "<p>Title : $title </p>";
-echo "<p>Post : $post </p>";
+//see if query runs successfully , establish query to database
+	//using single quote on the title to make sure it was being sent, its a string thats being stored
+
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'") ;
+//checking if a true value is stored within query 
+//if its true
+if($query) {
+echo "<p> Successfully inserted post: $title </p> ";
+}
+
+else{
+	echo "<p> $connection->error </p>";
+}
 
 $connection->close(); 
--- view/form.php
+++ view/form.php
@@ -11,10 +11,7 @@ require_once(__DIR__ . "/../model/config.php");
 <!-- this div is for title -->
 <div>
 <!-- The <label> tag defines a label for an <input> element.The <label> element does not render 
-as anything special for the user. However, it provides a usability improvement for mouse users,
- because if the user clicks on the text within the <label> element, it toggles the control.
-The for attribute of the <label> tag should be equal to the id attribute of the 
-related element to bind them together. --> 
+as anything special for the user.  --> 
 	<label for="title"> Title : </label>
 	<input type="text" name="title" />
 </div>
